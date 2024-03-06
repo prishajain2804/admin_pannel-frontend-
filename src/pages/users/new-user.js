be12343a37f845b04fc9d659d8fef56a0f6e5b11
prishajain2804 <prishajain028@gmail.com>
2024-03-06 13:19:44 +0530

    commit

--- src/pages/users/new-user.js
+++ src/pages/users/new-user.js
@@ -2,7 +2,23 @@ import React from 'react'
 
 const NewUser= () => {
   return (
-    <h1>I am new user</h1>
+
+    <form>
+      <label name="ID">Enter ID:</label>
+      <input type="text" id="ID"></input><br></br>
+      <label name="username">Enter UserName:</label>
+      <input type="text" id="username"></input><br></br>
+      <label name="password">Enter Password:</label>
+      <input type="text" id="password"></input><br></br>
+      <label name="firstname">Enter FirstName:</label>
+      <input type="text" id="firstname"></input><br></br>
+      <label name="lastname">Enter lastname:</label>
+      <input type="text" id="lastname"></input><br></br>
+      <label name="created">Created:</label>
+      <input type="text" id="created"></input><br></br>
+      <label name="modified">Modified:</label>
+      <input type="text" id="modified"></input>
+    </form>
   )
 }
 
--- src/pages/users/user-list.js
+++ src/pages/users/user-list.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "./user-list.module.css"
 
 const UserList = () => {
+
+
+  const [users,setUsers] = useState([]);
+
+  useEffect(()=>{
+   loadData();
+  },[]);
+
+  const loadData = async() =>{
+    const response = await fetch("http://localhost:3001/users");
+    const responseJSON = await response.json();
+    
+    console.log(responseJSON)
+    setUsers(responseJSON);
+  }
   return (
     <>
     <h1>I am UserList</h1>
@@ -11,12 +26,25 @@ const UserList = () => {
             <th>UserName</th>
             <th>Password</th>
             <th>Email</th>
-            <th>FirstName</th>
+            <th>First Name</th>
             <th>Last Name</th>
             <th>Created</th>
             <th>Modified</th>
         </tr>
 
+        {users.map((users)=>{
+          return (
+            <tr>
+          <td>{users.id}</td>
+          <td>{users.username}</td>
+          <td>{users.password}</td>
+          <td>{users.email}</td>
+          <td>{users.first_name}</td>
+          <td>{users.last_name}</td>
+          <td>{users.created}</td>
+          <td>{users.modified}</td>
+          </tr>
+        )})}
     </table>
     </>
   )
